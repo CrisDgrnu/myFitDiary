1bff8319d3f2f5e18108f0764e1bd52c2a511b7f
CrisDgrnu <cfres@127.0.0.1localhost>
2021-06-18 14:06:15 +0200

    Swagger config file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// Json parser
+app.use(express.json());
+
+// Documentation
+const swagger = require('./config/docs/Swagger');
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+// Routes
 const userRoute = require('./api/routes/UserRoute');
 const exerciseRoute = require('./api/routes/ExerciseRoute');
 const workoutRoute = require('./api/routes/WorkoutRoute');
 
-app.use(express.json());
-
 app.use('/users', userRoute);
 app.use('/exercises', exerciseRoute);
 app.use('/workouts', workoutRoute);
+
 
 
 app.listen(PORT, () => {
--- src/api/routes/UserRoute.js
+++ src/api/routes/UserRoute.js
@@ -6,6 +6,18 @@ const userSchema = require('../validations/UserValidation');
 
 const userController = require('../controllers/UserController');
 
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     description: Create a new user
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400: 
+ *          description: Bad request
+ * 
+ */
 router.post('/', validator(userSchema), userController.createUser);
 
 module.exports = router;
