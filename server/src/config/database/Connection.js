e8e1d821dd481babc464bb909183be83143d5d4f
CrisDgrnu <cfres@127.0.0.1localhost>
2021-06-23 00:08:04 +0200

    Refactor from mariaDB to mongoDB using moongose

--- server/src/config/database/Connection.js
+++ server/src/config/database/Connection.js
@@ -1,20 +1,15 @@
-const Sequelize = require('sequelize');
+const mongoose = require('mongoose');
+const connectionString = 'mongodb+srv://cfres:rJYbtYzM67EMRtb@cluster0.4vbrj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const connection = new Sequelize('myFitDiary','root','1234',{
-    dialect: 'mariadb',
-    host: 'localhost',
-    logging: false
+
+mongoose.connect(connectionString,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    console.log('Connected to the database');
+}).catch(() => {
+    console.log('Error connecting to the database');
 });
-
-connection.sync({ force: true })
-
-module.exports = connection;
-
-
-
-
-
-
-
-
 
