506a63eab152f726e3485d9eb4827d216231e494
CrisDgrnu <cfres@127.0.0.1localhost>
2021-06-23 00:09:42 +0200

    Controllers refactor due to mongoDB

--- server/src/api/controllers/ExerciseController.js
+++ server/src/api/controllers/ExerciseController.js
@@ -1,13 +1,4 @@
-const db = require('../../config/database/Entities');
-
 exports.createExercise = async (req, res) => {
     const body = req.body;
-    
-    try {
-        const exercise = await db.Exercise.create(body);
-        res.status(201).json({id: exercise.id, name: exercise.name});
-    } catch (error) {
-        const name = body.name;
-        res.status(409).json({ error: 'This exercise already exists', name });
-    }
+    res.json(body);
 };
--- server/src/api/controllers/UserController.js
+++ server/src/api/controllers/UserController.js
@@ -1,16 +1,9 @@
-const db = require('../../config/database/Entities');
-
 const encryptPassword = require('../helpers/PasswordBcrypt');
+const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
-    const body = req.body;
-
-    try {
-        body.password = encryptPassword(body.password);
-        const user = await db.User.create(body);
-        res.status(201).json({ message: 'User created correctly', userId: user.id });
-    } catch (error) {
-        const email = body.email;
-        res.status(409).json({ error: 'This email already exists', email });
-    }
+    req.body.password = encryptPassword(req.body.password);
+    User.create(req.body).then((user) => {
+        res.json(user);
+    });
 };
