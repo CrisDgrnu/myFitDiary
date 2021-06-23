fe3f57435ff5311d1bbede2568086ec38b729af2
CrisDgrnu <cfres@127.0.0.1localhost>
2021-06-23 14:22:31 +0200

    User schema changes to fit the new requests

--- server/src/api/models/User.js
+++ server/src/api/models/User.js
@@ -1,12 +1,25 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
+    username: String,
     name: String,
     surname: String,
     age: Number,
     weight: Number,
-    email: String,
+    email: {
+        type: String,
+        unique: true
+    },
     password: String,
+});
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id;
+        delete returnedObject._id;
+        delete returnedObject.__v;
+        delete returnedObject.password;
+    }
 });
 
 const User = model('User', userSchema);
